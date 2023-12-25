17791df4bd57d703cabaa562cef12e5354151d73
WorthyFire <worthyfire@vk.com>
2023-12-25 22:09:58 +0700

    add something

--- main/models.py
+++ main/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class AdvUser(AbstractUser):
+    basket = models.ManyToManyField('Product', blank=True, related_name='basket')
 
     class Meta:
         pass
@@ -9,6 +10,7 @@ class AdvUser(AbstractUser):
 class Product(models.Model):
     description = models.TextField(verbose_name='Описание товара')
     photo = models.ImageField(upload_to='media/', default=None, verbose_name='Фото товара')
+
 
 
 
